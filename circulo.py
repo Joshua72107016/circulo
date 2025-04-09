c0bb758084bfc13458abb5a3ff56c60f4b56fc26
72107016 <72107016@continental.edu.pe>
2025-04-09 11:38:21 -0500

    Se implemento otra rama

--- circulo.py
+++ circulo.py
@@ -1,10 +1,17 @@
 class Circulo:
     PI = 3.1416
-    def __init__(self,radio):
+
+    def __init__(self, radio):
         self.radio = radio
+
     def circunferencia(self):
         return 2 * self.PI * self.radio
+
+    def area(self):
+        return self.PI * self.radio * self.radio
+
 
 if __name__ == "__main___":
     instancia_circulo = Circulo(10)
     print(f"La cicurferencia es: {instancia_circulo.circunferencia()}")
+    print(f"El area es: {instancia_circulo.area()}")
