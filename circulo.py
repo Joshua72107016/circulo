47e944e9df3a095871ff7ded787913b17fa6d233
72107016 <72107016@continental.edu.pe>
2025-04-09 11:19:12 -0500

    Se implementó la circunferencia

---
+++ circulo.py
@@ -0,0 +1,10 @@
+class Circulo:
+    PI = 3.1416
+    def __init__(self,radio):
+        self.radio = radio
+    def circunferencia(self):
+        return 2 * self.PI * self.radio
+
+if __name__ == "__main___":
+    instancia_circulo = Circulo(10)
+    print(f"La cicurferencia es: {instancia_circulo.circunferencia()}")
